672c96cf5b672309336bc027aa4aff39e543aaab
lbbboy <lbbboy@gmail.com>
2026-08-30 03:13:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,5 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # NSS
-echo 'src-git nss_packages https://github.com/qosmio/nss-packages.git' >>feeds.conf.default
-echo 'src-git sqm_scripts_nss https://github.com/qosmio/sqm-scripts-nss.git' >>feeds.conf.default
+#echo 'src-git nss_packages https://github.com/qosmio/nss-packages.git' >>feeds.conf.default
+#echo 'src-git sqm_scripts_nss https://github.com/qosmio/sqm-scripts-nss.git' >>feeds.conf.default
