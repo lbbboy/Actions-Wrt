8db94c33c5781b5ea3c9d989b7abdf863a824743
lbbboy <lbbboy@gmail.com>
2026-09-01 16:14:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,16 +21,6 @@
 #echo 'src-git nss_packages https://github.com/qosmio/nss-packages.git' >>feeds.conf.default
 #echo 'src-git sqm_scripts_nss https://github.com/qosmio/sqm-scripts-nss.git' >>feeds.conf.default
 
-function set_kernel_size() {
-  #修改jdc ax1800 pro 的内核大小为12M
-  image_file='./target/linux/qualcommax/image/ipq60xx.mk'
-  sed -i "/^define Device\/emmc-common/,/^endef/ s/KERNEL_SIZE := 6144k/KERNEL_SIZE := 12288k/" $image_file
-  sed -i "/^define Device\/nand-common/,/^endef/ s/^endef/\tKERNEL_SIZE := 8192k\nendef/" $image_file
-  sed -i "/^define Device\/jdcloud_re-ss-01/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
-  sed -i "/^define Device\/jdcloud_re-cs-02/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
-  sed -i "/^define Device\/jdcloud_re-cs-07/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
-  sed -i "/^define Device\/link_nn6000-common/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
-  sed -i "/^define Device\/linksys_mr/,/^endef/ { /KERNEL_SIZE := 8192k/s//KERNEL_SIZE := 12288k/ }" $image_file
-  sed -i "/^define Device\/linksys_mr7350/,/^endef/ s/^endef/\tIMAGE_SIZE := 12288k\nendef/" $image_file
-  cat $image_file
-}
+ #修改jdc ax1800 pro 的内核大小为12M
+image_file='./target/linux/qualcommax/image/ipq60xx.mk'
+sed -i "/^define Device\/jdcloud_re-ss-01/,/^endef/ s/KERNEL_SIZE\s*:=\s*6144k/KERNEL_SIZE := 12288k/" $image_file
